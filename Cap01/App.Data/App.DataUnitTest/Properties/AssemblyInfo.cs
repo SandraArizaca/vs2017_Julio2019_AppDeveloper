da0f8a58d0d0bd2d35233281d8e7afb1ae4a0a50
SandraArizaca <sandra.arizaca@gmail.com>
2019-07-17 22:14:13 -0500

    Cap02

---
+++ Cap01/App.Data/App.DataUnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("App.DataUnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("App.DataUnitTest")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("7b4dd18b-c5ba-4024-b46e-ddec40aacbd8")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Cap01/App.Data/App.DataUnitTest/SolicitudUnitTest.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using App.Data;
+using App.Data.Base;
+
+namespace App.DataUnitTest
+{
+    [TestClass]
+    public class SolicitudUnitTest
+    {
+        [TestMethod]
+        public void SolicitudHWTest()
+        {
+            //SolicitudAcceso obj1 = new SolicitudAcceso();
+            //obj1.NivelAcceso = 2;
+            SolicitudAcceso obj1 = new SolicitudAcceso { NivelAcceso = 2 };
+
+            Assert.IsTrue(obj1.Aprobar(), "El usuario administrador no puede aprobar");
+
+            Solicitud obj2 = new SolicitudAcceso { NivelAcceso = 4 };
+            Assert.IsTrue(obj2.Aprobar(), "El usuario administrador no puede aprobar");
+        }
+
+        [TestMethod]
+        public void SolicitudesTest()
+        {
+            SolicitudHardware obj1 = new SolicitudHardware { Fecha = DateTime.Now};
+            Assert.IsTrue(VerificarAprobacion(obj1), "El usuario administrador no puede aprobar");
+        }
+
+        [TestMethod]
+        public void Solicitudes2Test()
+        {
+            SolicitudAcceso obj1 = new SolicitudAcceso { Fecha = DateTime.Now, NivelAcceso = 5 };
+            Assert.IsTrue(VerificarAprobacion(obj1), "El usuario administrador no puede aprobar");
+        }
+
+        private bool VerificarAprobacion(Solicitud solicitud)
+        {
+            return solicitud.Aprobar();
+        }
+    }
+}
