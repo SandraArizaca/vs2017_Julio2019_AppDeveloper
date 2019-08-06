83001aad5899be100ad04a4b381534757296f6dd
SandraArizaca <sandra.arizaca@gmail.com>
2019-08-05 22:25:53 -0500

    20190805

---
+++ EjercicioEntityFramework/EjercicioEntityFramework/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjercicioEntityFramework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var db = new dbUniversidadEntities())
+            {
+                db.Facultad.Add(new Facultad()
+                {
+                    Nombre = "Ciencias de la Computacion"
+                });
+                db.SaveChanges();
+
+                var alumno = new Alumno()
+                {
+                    Id = 1,
+                    Nombre = "Sandra Arizaca",
+                    FechaDeNacimiento = DateTime.Now.Date,
+                    Curso = new List<Curso>()
+                    {
+                        new Curso() { Id = 1, Nombre = "Visual Basic", FacultadId = 1}
+                    }
+                };
+
+                db.Alumno.Add(alumno);
+                db.SaveChanges();
+            }
+        }
+    }
+}
