58707a099988d843b3d15abcd1d642e04a15ec0b
SandraArizaca <sandra.arizaca@gmail.com>
2019-08-21 22:31:26 -0500

    20190821_2231

--- VisualStudio67-master/Shop.Presentation.WebPage/Scripts/LogicaDeNegocios/MantenimientoDeProductos.js
+++ VisualStudio67-master/Shop.Presentation.WebPage/Scripts/LogicaDeNegocios/MantenimientoDeProductos.js
@@ -3,9 +3,12 @@
     data: {
         categories: [],
         categoryId: null,
+        categoryIdFilter:0,
         nombre: '',
+        nombreFiltro:'',
         descripcion: '',
-        listaDeProductos:[]
+        listaDeProductos: [],
+        listaDeProductosSinFiltrar:[]
     },
     methods: {
         GetCategoriesFromServer: function () {
@@ -20,11 +23,11 @@
             this.categoryId = null;
         },
         RegistrarProductoEnElServidor: function () {
-            post('/api/productmanagement/RegisterProduct', {
-                Name: this.nombre,
-                Descripcion: this.descripcion,
-                CategoryId: this.categoryId
-            })
+                post('/api/productmanagement/RegisterProduct', {
+                    Name: this.nombre,
+                    Descripcion: this.descripcion,
+                    CategoryId: this.categoryId
+                })
                 .then(newProduct => {
                     this.listaDeProductos.push(newProduct);
                     this.ResetearInterface();
@@ -34,7 +37,33 @@
             $.get('/api/productmanagement/GetAllProducts')
                 .then(productosDelServidor => {
                     this.listaDeProductos.push(...productosDelServidor)
+                    this.listaDeProductosSinFiltrar.push(...productosDelServidor);
                 });
+        },
+        cambioElFiltro: function () {
+            if (this.categoryIdFilter == 0) {
+                this.listaDeProductos = [];
+                this.CargarProductosDelServidor();
+            }
+            else {
+                $.get('/api/productmanagement/GetProductByCategoryId?categoryId=' + this.categoryIdFilter)
+                    .then(productosDelServidor => {
+                        this.listaDeProductos = [];
+                        this.listaDeProductos.push(...productosDelServidor)
+                    });
+            }
+        },
+        ActivarFiltro: function () {
+            if (this.nombreFiltro.length > 0) {
+                this.listaDeProductos = [];
+                this.listaDeProductos = this.listaDeProductosSinFiltrar.filter(x => {
+                    return x.Name.includes(this.nombreFiltro);
+                });
+            }
+            else {
+                this.listaDeProductos = this.listaDeProductosSinFiltrar;
+            }
+
         }
     }
 })
